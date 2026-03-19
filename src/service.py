c495e89688b771a54c3d2c4dcfe35116a5bf2bd6
joinercantillo916 <jcantill55@gmail.com>
2026-03-19 18:32:23 -0500

    service.py added

--- src/service.py
+++ src/service.py
@@ -1,5 +1,12 @@
-def lsit_tasks(): 
-    for i, j in enumerate(task, start=1):
-        print(f'task #{i + 1} . NAME:{j['name']} STATE: {j['state']} PRIORITY: {j['priority']} \n')
-    else:
-        print('empty list')
+
+def list_tasks():
+    task = {}
+    option = -1
+    if option == 3:
+
+        for i, j in enumerate(task, start=1):
+            print(f'task #{i + 1} . NAME:{j['name']} STATE: {j['state']} PRIORITY: {j['priority']} \n')
+        
+        
+        else:
+            print("empty list")

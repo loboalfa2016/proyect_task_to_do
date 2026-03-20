75df0025c12e2e2c4c08b05bb996eb39a79826f2
diazvergaraarley <diazvergara.arley@gmail.com>
2026-03-19 20:09:48 -0500

    Updated

--- src/menu.py
+++ src/menu.py
@@ -27,15 +27,8 @@ def start_menu():
             elif option == 4:
                 print("Delete task Selected")
             elif option == 5:
-                from service import task, filter_tasks_by_status
-                search_status = input("Enter the status to filter by (Pending, In Progress, Completed): ")
-                filtered_tasks = filter_tasks_by_status(task, search_status)
-                if filtered_tasks:
-                    print("Filtered tasks:")
-                    for i, t in enumerate(filtered_tasks, start=1):
-                        print(f"{i}. Title: {t['title']}, Description: {t['description']}, Priority: {t['priority']}, Status: {t['status']}")
-                else:
-                    print("No tasks found with that status.")
+                from service import filter_tasks_by_status
+                filter_tasks_by_status
             elif option == 6:
                 print("Goodbye")
                 break

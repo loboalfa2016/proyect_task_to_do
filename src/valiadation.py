f1d9ae09c89868dd1363bbc645c6c847072f7d09
joinercantillo <jcantill55@gmail.com>
2026-03-20 17:16:39 -0500

    update readme

---
+++ src/valiadation.py
@@ -0,0 +1,8 @@
+def list_task():
+    validation = -1
+while validation != 0:
+    print('welcome To-Do Pro')
+    options = {'1': 'Add Task', '2': 'View Tasks', '3': 'Edit Task', '4': 'Delete Task', '5': 'Filter Tasks by Status', '0': 'Exit'}
+    for key, value in options.items():
+        print(f"{key} - - {value} - - ")
+    validation = int(input("Enter your choice: "))

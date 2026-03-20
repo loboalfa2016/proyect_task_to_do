10264ce0271a727b825d308a1566b2d7c7a2c324
joinercantillo916 <jcantill55@gmail.com>
2026-03-19 19:15:58 -0500

    Update

--- src/service.py
+++ src/service.py
@@ -1,10 +1,9 @@
-
-def list_tasks():
-    task = []
-    option = -1
-    if option == 3:
-        if len(task) > 0:
-            for i, j in enumerate(task, start=1):
-                print(f'task #{i + 1} . NAME:{j['name']} STATE: {j['state']} PRIORITY: {j['priority']} \n')
-        else:
-            print("empty list")
+task = []
+option = -1
+if option == 3:
+    print("the task list is: \n")
+    if len(task) > 0:
+        for i, j in enumerate(task):
+            print(f'task #{i + 1} . NAME:{j["name"]} STATE: {j["state"]} PRIORITY: {j["priority"]} \n')
+    else:
+        print("empty list")
